3d088d855b713e693a27f8ddac8084478006f9a2
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2023-08-06 00:37:40 +0200

    Sobre tarifa structure added , pictures missing

--- we-tarifa-app/src/components/SobreTarifa.js
+++ we-tarifa-app/src/components/SobreTarifa.js
@@ -1,9 +1,40 @@
 import React from 'react'
 
+import './styles/SobreTarifa.css'
+
 const SobreTarifa = () => {
   return (
-    <div>
-      Sobre Tarifa Site
+    <div className='SobreTarifa'>
+
+      <h1>Sobre Tarifa</h1>
+
+      <div className='content'>
+
+          <div>
+              <img></img>
+              <img></img>
+              <img></img>
+          </div>
+
+          <div>
+            Tarifa, situada en la provincia de Cádiz, es una encantadora localidad costera que te cautivará con su belleza natural, su ambiente relajado y su rica historia. Ubicada en el punto más meridional de la península ibérica, en el estrecho de Gibraltar, esta pintoresca ciudad ofrece una mezcla única de culturas y tradiciones.
+
+            Sus playas de arena dorada y aguas cristalinas son uno de sus mayores atractivos. Tarifa es famosa por ser un paraíso para los amantes de los deportes acuáticos, como el windsurf y el kitesurf, gracias a los fuertes vientos que soplan en la zona. La Playa de Los Lances y la Playa de Valdevaqueros son dos de las playas más conocidas para practicar estos emocionantes deportes.
+
+            El casco antiguo de Tarifa te transportará a la época medieval, con sus estrechas calles empedradas y casas encaladas. En el corazón del casco antiguo se encuentra la Plaza de Santa María, rodeada de encantadoras cafeterías y bares donde puedes disfrutar de la auténtica gastronomía gaditana.
+
+            Uno de los lugares más emblemáticos de Tarifa es el Castillo de Guzmán el Bueno, una fortaleza medieval situada en el punto más alto de la ciudad. Desde aquí, se puede disfrutar de unas impresionantes vistas panorámicas del mar y del casco antiguo.
+
+            Tarifa también es conocida por su rica historia y sus vestigios arqueológicos, como las ruinas romanas de Baelo Claudia, ubicadas cerca de la playa de Bolonia. Estos restos arqueológicos ofrecen una fascinante mirada al pasado romano de la región.
+
+            Además de sus atractivos naturales e históricos, Tarifa es un lugar de gran importancia para la observación de aves migratorias. El Parque Natural del Estrecho es un santuario para numerosas especies de aves que cruzan entre Europa y África durante sus migraciones.
+
+            Tarifa también es conocida por su ambiente relajado y su estilo de vida bohemio. Sus calles están llenas de tiendas de artesanía, boutiques y galerías de arte, lo que le da un encanto especial.
+
+            En resumen, Tarifa es un destino fascinante que combina playas de ensueño, deportes acuáticos emocionantes, historia medieval, naturaleza impresionante y una cultura auténtica. Si buscas un lugar donde disfrutar de la belleza del mar y la tranquilidad, mientras te sumerges en la historia y cultura andaluza, Tarifa es el lugar ideal para ti.
+          </div>
+      </div>
+
     </div>
   )
 }
